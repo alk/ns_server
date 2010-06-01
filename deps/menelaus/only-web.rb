45743d9772828df12151206dcd65471720b903b8
Aliaksey Kandratsenka <alk@tut.by>
2010-06-01 11:46:21 +0300

    mark
    Change-Id: I83a78ceb683333a1c0a72b05dd040b900ca109ec

--- deps/menelaus/only-web.rb
+++ deps/menelaus/only-web.rb
@@ -37,15 +37,7 @@ class Middleware
 
   def call(env)
     req = Rack::Request.new(env)
-    if req.path_info == '/js/t-all.js'
-      begin
-        sh "sprocketize -I priv/js priv/js/app.js priv/js/hooks.js >priv/public/js/t-all.js"
-      rescue
-        status, headers, body = @app.call(env)
-        body = ["alert('#{escape_javascript($!.inspect)}')"]
-        return [200, {'Content-Type' => 'application/javascript'}, body]
-      end
-    elsif req.path_info == "/index.html"
+    if req.path_info == "/index.html"
       replacement = "/js/t-all.js\""
       if $DEVMODE
         files = IO.readlines('priv/js/app.js').grep(%r|//= require <(.*)>$|) {|_d| $1}
@@ -55,12 +47,6 @@ class Middleware
       end
       text = IO.read($DOCROOT + "/index.html").gsub(Regexp.compile(Regexp.escape("/js/all.js\"")), replacement)
       return [200, {'Content-Type' => 'text/html; charset=utf-8'}, [text]]
-    elsif req.path_info.starts_with?('/js/')
-      path = req.path_info
-      apath = "priv/" + path
-      if File.file?(apath)
-        return [200, {'Content-Type' => 'application/javascript'}, IO.readlines(apath, 'r')]
-      end
     end
 
     @app.call(env)
