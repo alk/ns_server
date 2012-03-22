55eeaa49fc08f76a090e8d288d3f43f6454501cd
Aliaksey Kandratsenka <alk@tut.by>
2012-03-22 16:01:35 -0700

    Revert "Implementation of delete-with-meta in capi"
    This reverts commit fd4e3272a0b478901c086bf32aa14b9fd0331a8f.

--- src/capi_replication.erl
+++ src/capi_replication.erl
@@ -202,8 +202,8 @@ do_update_replicated_doc_loop(Bucket, VBucket, DocId,
             {error, enoent, CAS} ->
                 case DocDeleted of
                     true ->
-                        do_delete_with_meta(Bucket, DocId, VBucket, DocRev,
-                                            CAS);
+                        %% TODO: we must preserve source revision here
+                        ok;
                     false ->
                         do_set_with_meta(Bucket, DocId, VBucket, DocValue,
                                          DocRev, CAS)
@@ -218,12 +218,19 @@ do_update_replicated_doc_loop(Bucket, VBucket, DocId,
                     ours ->
                         ok;
                     theirs ->
-                        {cas, CAS} = lists:keyfind(cas, 1, Props),
                         case DocDeleted of
                             true ->
-                                do_delete_with_meta(Bucket, DocId, VBucket,
-                                                    DocRev, CAS);
+                                case Deleted of
+                                    true ->
+                                        %% TODO: we must preserve winning
+                                        %% revision here
+                                        ok;
+                                    false ->
+                                        {cas, CAS} = lists:keyfind(cas, 1, Props),
+                                        do_delete(Bucket, DocId, VBucket, CAS)
+                                end;
                             false ->
+                                {cas, CAS} = lists:keyfind(cas, 1, Props),
                                 do_set_with_meta(Bucket, DocId, VBucket,
                                                  DocValue, DocRev, CAS)
                         end
@@ -254,16 +261,18 @@ do_set_with_meta(Bucket, DocId, VBucket, DocValue, DocRev, CAS) ->
             {error, {bad_request, einval}}
     end.
 
-do_delete_with_meta(Bucket, DocId, VBucket, DocRev, CAS) ->
-    case ns_memcached:delete_with_meta(Bucket, DocId, VBucket, {revid, DocRev},
-                                       CAS) of
-        {ok, _, _} ->
+do_delete(Bucket, DocId, VBucket, CAS) ->
+    {ok, Header, _Entry, _NCB} =
+        ns_memcached:delete(Bucket, DocId, VBucket, CAS),
+    Status = Header#mc_header.status,
+    case Status of
+        ?SUCCESS ->
             ok;
-        {memcached_error, key_enoent, _} ->
+        ?KEY_ENOENT ->
             retry;
-        {memcached_error, not_my_vbucket, _} ->
+        ?NOT_MY_VBUCKET ->
             {error, {bad_request, not_my_vbucket}};
-        {memcached_error, einval, _} ->
+        ?EINVAL ->
             {error, {bad_request, einval}}
     end.
 
