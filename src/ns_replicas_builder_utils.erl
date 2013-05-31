ae0efc450636c489274707dc339065793bb6ca71
Aliaksey Kandratsenka <alk@tut.by>
2013-05-31 09:08:01 -0700

    mark
    Change-Id: Ib36d7b2f38e6c5af0940c3d81acf9f7f481677b0

--- src/ns_replicas_builder_utils.erl
+++ src/ns_replicas_builder_utils.erl
@@ -25,49 +25,56 @@ tap_name(VBucket, _SrcNode, DstNode) ->
     lists:flatten(io_lib:format("building_~p_~p", [VBucket, DstNode])).
 
 kill_a_bunch_of_tap_names(Bucket, Node, TapNames) ->
-    ns_memcached:kill_a_bunch_of_tap_names(Bucket, Node, TapNames).
+    case ns_config_ets_dup:unreliable_read_key(spend_sockets_on_kinda_fast_kill_a_bunch_of_tap_names, undefined) of
+        undefined ->
+            ns_memcached:kill_a_bunch_of_tap_names(Bucket, Node, TapNames);
+        _ ->
+            kill_a_bunch_of_tap_names_old_style(Bucket, Node, TapNames)
+    end.
 
-    %% Config = ns_config:get(),
-    %% User = ns_config:search_node_prop(Node, Config, memcached, admin_user),
-    %% Pass = ns_config:search_node_prop(Node, Config, memcached, admin_pass),
-    %% McdPair = {Host, Port} = ns_memcached:host_port(Node),
-    %% {ok, Sock} = gen_tcp:connect(Host, Port, [binary,
-    %%                                           {packet, 0},
-    %%                                           {active, false},
-    %%                                           {nodelay, true},
-    %%                                           {delay_send, true}]),
-    %% UserBin = mc_binary:bin(User),
-    %% PassBin = mc_binary:bin(Pass),
-    %% SenderPid = spawn_link(fun () ->
-    %%                                ok = mc_binary:send(Sock, req, #mc_header{opcode = ?CMD_SASL_AUTH},
-    %%                                                    #mc_entry{key = <<"PLAIN">>,
-    %%                                                              data = <<UserBin/binary, 0:8,
-    %%                                                                       UserBin/binary, 0:8,
-    %%                                                                       PassBin/binary, 0:8>>}),
-    %%                                ok = mc_binary:send(Sock, req, #mc_header{opcode = ?CMD_SELECT_BUCKET},
-    %%                                                    #mc_entry{key = iolist_to_binary(Bucket)}),
-    %%                                [ok = mc_binary:send(Sock, req, #mc_header{opcode = ?CMD_DEREGISTER_TAP_CLIENT}, #mc_entry{key = TapName})
-    %%                                 || TapName <- TapNames]
-    %%                        end),
-    %% try
-    %%     {ok, #mc_header{status = ?SUCCESS}, _} = mc_binary:recv(Sock, res, 50000), % CMD_SASL_AUTH
-    %%     {ok, #mc_header{status = ?SUCCESS}, _} = mc_binary:recv(Sock, res, 50000), % CMD_SELECT_BUCKET
-    %%     [{ok, #mc_header{status = ?SUCCESS}, _} = mc_binary:recv(Sock, res, 50000) || _TapName <- TapNames]
-    %% after
-    %%     erlang:unlink(SenderPid),
-    %%     erlang:exit(SenderPid, kill),
-    %%     misc:wait_for_process(SenderPid, infinity)
-    %% end,
-    %% ?log_info("Killed the following tap names on ~p: ~p", [Node, TapNames]),
-    %% ok = gen_tcp:close(Sock),
-    %% [master_activity_events:note_deregister_tap_name(Bucket, McdPair, AName) || AName <- TapNames],
-    %% receive
-    %%     {'EXIT', SenderPid, Reason} ->
-    %%         normal = Reason
-    %% after 0 ->
-    %%         ok
-    %% end,
-    %% ok.
+kill_a_bunch_of_tap_names_old_style(Bucket, Node, TapNames) ->
+
+    Config = ns_config:get(),
+    User = ns_config:search_node_prop(Node, Config, memcached, admin_user),
+    Pass = ns_config:search_node_prop(Node, Config, memcached, admin_pass),
+    McdPair = {Host, Port} = ns_memcached:host_port(Node),
+    {ok, Sock} = gen_tcp:connect(Host, Port, [binary,
+                                              {packet, 0},
+                                              {active, false},
+                                              {nodelay, true},
+                                              {delay_send, true}]),
+    UserBin = mc_binary:bin(User),
+    PassBin = mc_binary:bin(Pass),
+    SenderPid = spawn_link(fun () ->
+                                   ok = mc_binary:send(Sock, req, #mc_header{opcode = ?CMD_SASL_AUTH},
+                                                       #mc_entry{key = <<"PLAIN">>,
+                                                                 data = <<UserBin/binary, 0:8,
+                                                                          UserBin/binary, 0:8,
+                                                                          PassBin/binary, 0:8>>}),
+                                   ok = mc_binary:send(Sock, req, #mc_header{opcode = ?CMD_SELECT_BUCKET},
+                                                       #mc_entry{key = iolist_to_binary(Bucket)}),
+                                   [ok = mc_binary:send(Sock, req, #mc_header{opcode = ?CMD_DEREGISTER_TAP_CLIENT}, #mc_entry{key = TapName})
+                                    || TapName <- TapNames]
+                           end),
+    try
+        {ok, #mc_header{status = ?SUCCESS}, _} = mc_binary:recv(Sock, res, 50000), % CMD_SASL_AUTH
+        {ok, #mc_header{status = ?SUCCESS}, _} = mc_binary:recv(Sock, res, 50000), % CMD_SELECT_BUCKET
+        [{ok, #mc_header{status = ?SUCCESS}, _} = mc_binary:recv(Sock, res, 50000) || _TapName <- TapNames]
+    after
+        erlang:unlink(SenderPid),
+        erlang:exit(SenderPid, kill),
+        misc:wait_for_process(SenderPid, infinity)
+    end,
+    ?log_info("Killed the following tap names on ~p: ~p", [Node, TapNames]),
+    ok = gen_tcp:close(Sock),
+    [master_activity_events:note_deregister_tap_name(Bucket, McdPair, AName) || AName <- TapNames],
+    receive
+        {'EXIT', SenderPid, Reason} ->
+            normal = Reason
+    after 0 ->
+            ok
+    end,
+    ok.
 
 kill_tap_names(Bucket, VBucket, SrcNode, DstNodes) ->
     kill_a_bunch_of_tap_names(Bucket, SrcNode,
