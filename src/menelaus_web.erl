2a1933f20a594e99560058f8c41cce0b869aa1eb
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-08-13 03:10:05 -0700

    MB-8663 Add REST APIs for working with global xdcr settings.
    For consistency. Supported parameters are all the same plus
    enableExtraDebug.
    
    Change-Id: I632ba27de5ab2a5ffe19c4ad675e66604accc68f

--- src/menelaus_web.erl
+++ src/menelaus_web.erl
@@ -242,6 +242,8 @@ loop_inner(Req, AppRoot, DocRoot, Path, PathTokens) ->
                              {auth_ro, fun handle_settings_view_update_daemon/1};
                          ["settings", "autoCompaction"] ->
                              {auth_ro, fun handle_settings_auto_compaction/1};
+                         ["settings", "replications"] ->
+                             {auth_ro, fun menelaus_web_xdc_replications:handle_global_replication_settings/1};
                          ["settings", "replications", XID] ->
                              {auth_ro, fun menelaus_web_xdc_replications:handle_replication_settings/2, [XID]};
                          ["internalSettings"] ->
@@ -323,6 +325,8 @@ loop_inner(Req, AppRoot, DocRoot, Path, PathTokens) ->
                              {auth, fun handle_settings_view_update_daemon_post/1};
                          ["settings", "readOnlyUser"] ->
                              {auth, fun handle_settings_read_only_user_post/1};
+                         ["settings", "replications"] ->
+                             {auth, fun menelaus_web_xdc_replications:handle_global_replication_settings_post/1};
                          ["settings", "replications", XID] ->
                              {auth, fun menelaus_web_xdc_replications:handle_replication_settings_post/2, [XID]};
                          ["internalSettings"] ->
